f8c06e04accb5e5a0e3ab7730a6ec4ff63a1151a
Jesse Boyer <imboyus@gmail.com>
2016-06-11 00:17:29 -0400

    Facade in progress

---
+++ facade/Facade.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Facade
+ * @desc This is good for have a "Base" class that calls the same methods
+ *       on other classes which do the same thing but with minor tweaks.
+ *       A good example would be figuring out a Retail price based on 
+ *       a product.
+ *
+ */
+class Facade // Should be called RetailPriceFacade
+{
+    public $item_id;
+    public $vendor_id;
+
+    public function __construct($item_id, $vendor_id) {
+        $this->item_id = $item_id;
+        $this->vendor_id = $vendor_id;
+    }
+
+    // This would be the shared function among all the other classes
+    public function process() {
+        $wholesale_price = new WholesaleCost($this->item_id, $this->vendor_id);
+        $markup_price = new MarkupCost($this->item_id, $this->vendor_id);
+        return $wholesale_price + $markup_price;
+    }
+
+}
+
+class WholesaleCost
+{
+    public function __construct($item_id, $vendor_id) {
+        // Database look up the vendors wholesale price
+        return 40.00;
+    }
+}
+
+class MarkupCost
+{
+    // This could obviously do much more
+    public function __construct($item_id, $vendor_id) {
+        // Database look up the vendors wholesale price
+        return 4.95;
+    }
+
+}
+
+/**
+ * Example
+ */
+// Here you would pss the item_id and vendor_id
+$toysrus = new Facade(1, 2);
+$kbtoys = new Facade(1, 3);
+$bestbuy = new Facad(1, 4);
+
+$retail_prices = [
+    'toysrus' => $toysrus->process();
+    'kbtoys' => $kbtoys->process();
+    'bestbuy' => $bestbuy->process();
+];
+
+// Sell the most expensive item
+arsort($retail_prices);
+$most_expnsive = $retail_prices[0];
+
